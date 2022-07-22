f4ce4d1f0959271d62cdec1d64158992329bb16c
Fabrice Berthet <berthet-fabrice@hotmail.fr>
2022-07-22 22:01:55 +0200

    articles getAll/Post

--- bjv-backend/src/main/java/com/bjv/bjvbackend/articles/ArticleController.java
+++ bjv-backend/src/main/java/com/bjv/bjvbackend/articles/ArticleController.java
@@ -1,12 +1,36 @@
 package com.bjv.bjvbackend.articles;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bjv.bjvbackend.contact.ContactModel;
+
 @RestController
 @RequestMapping("api/v1")
+@CrossOrigin("http://localhost:4200")
 public class ArticleController {
 	
+	@Autowired
 	private ArticleRepository articleRepository;
+	
+	@GetMapping("articles")
+	public List<Article> getAllArticles(){
+		
+		return articleRepository.findAll();
+	}
+	
+	@PostMapping("articles")
+	public Article sendEmail(@RequestBody Article article){
+		
+		return articleRepository.save(article);
+	}
 
+	
 }
